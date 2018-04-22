ca4d2b12de6d5ccf32868b876d7dfae92e079f79
Hermann Höhne <hoehermann@gmx.de>
2018-04-22 13:19:03 +0200

    Forward filenames with spaces properly in helper script.

--- mpegts_truncate.sh
+++ mpegts_truncate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zenity --question --text="Really truncate up to current position (byte ${3})?\n(Data will be deleted permanently.)" && ( error=$("$(dirname "$0")"/fcollapse $@ 2>&1) && exit 0 || zenity --error --title="fcollapse failed" --text="${error}" && exit 1 )
+zenity --question --text="Really truncate up to current position (byte ${3})?\n(Data will be deleted permanently.)" && ( error=$("$(dirname "$0")"/fcollapse "$@" 2>&1) && exit 0 || zenity --error --title="fcollapse failed" --text="${error}" && exit 1 )
